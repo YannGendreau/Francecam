0ad96eaaa13273e42279d24ce03445ccada4e8ff
YannGendreau <yanngendreau@gmail.com>
2021-01-18 16:42:18 +0100

    AJOUTE dossier images + fichiers images

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,43 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+// import './styles/liste.scss';
+// import './styles/film.scss';
+// import './styles/film2.scss';
+// import './styles/cartefilmfinal.scss';
 
-// start the Stimulus application
-import './bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import images
+require('../assets/images/francecam-bg.jpg');
+
+// js import 
+const imagesContext = require.context('../assets/images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
+imagesContext.keys().forEach(imagesContext);
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore! Edit me in assets/app.js');
+
+var tabs = document.querySelectorAll(".buttonContainer button");
+var tab_wraps = document.querySelectorAll(".tabPanel");
+
+tabs.forEach(function(tab, tab_index){
+	tab.addEventListener("click", function(){
+		tabs.forEach(function(tab){
+			tab.classList.remove("active");
+		})
+		tab.classList.add("active");
+
+		tab_wraps.forEach(function(content, content_index){
+			if(content_index == tab_index){
+				content.style.display = "block";
+			}
+			else{
+				content.style.display = "none";
+			}
+		})
+
+	})
+})
