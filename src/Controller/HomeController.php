56cee6f30115d388aef46aac70ce69ffb12b3fcd
YannGendreau <yanngendreau@gmail.com>
2021-02-09 13:19:07 +0100

    MODIFICATION homeController test de CRUD (effacé)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Film;
 use App\Entity\Gamme;
+use App\Entity\Camera;
 use App\Entity\Marque;
+use App\Entity\Modele;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,16 +22,30 @@ class HomeController extends AbstractController
         
         // $film = new Film;
         // $marque = new Marque;
+        // $modele = new Modele;
 
         // $film->setTitle('Symfony');
         // $film->setDuree(123);
         // $film->setSynopsis("C'est l'histoire d'un type qui apprend Symfony");
         // $film->setSortie(2021);
         // $film->addMarque($marque->setName('Canon'));
+
+       
+        $camera = new Camera;
+        
+
+        // $camera->setMarque($marque->getId(1));
+        // $camera->setModele($modele->getId(6));
+  
+
+
+
         // $entityManager = $this->getDoctrine()->getManager(); 
-        // $entityManager->persist($film);
+        // $entityManager->persist($camera);
         
         // $entityManager->flush();
+
+        dump($camera->getMarqueModele());
     
 
         return $this->render('home/index.html.twig', [
