87f264f87664a4f9e87acf34875d1c01061a6a10
YannGendreau <yanngendreau@gmail.com>
2021-02-22 22:18:33 +0100

    MODIF HOMEPAGE Loop cameras, loop films

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,9 @@ use App\Entity\Gamme;
 use App\Entity\Camera;
 use App\Entity\Marque;
 use App\Entity\Modele;
+use App\Repository\FilmRepository;
+use App\Repository\CameraRepository;
+use App\Repository\CamerasRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,40 +19,16 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function index(): Response
+    public function index(CamerasRepository $cameraRepository, FilmRepository $filmRepository): Response
     {
-
-        
-        // $film = new Film;
-        // $marque = new Marque;
-        // $modele = new Modele;
-
-        // $film->setTitle('Symfony');
-        // $film->setDuree(123);
-        // $film->setSynopsis("C'est l'histoire d'un type qui apprend Symfony");
-        // $film->setSortie(2021);
-        // $film->addMarque($marque->setName('Canon'));
-
-       
-        $camera = new Camera;
-        
-
-        // $camera->setMarque($marque->getId(1));
-        // $camera->setModele($modele->getId(6));
-  
-
-
-
-        // $entityManager = $this->getDoctrine()->getManager(); 
-        // $entityManager->persist($camera);
-        
-        // $entityManager->flush();
-
-        dump($camera->getMarqueModele());
-    
+        $cameras = $cameraRepository->findAll();
+        $films = $filmRepository->findAll();
 
         return $this->render('home/index.html.twig', [
-            
+            'cameras' => $cameras,
+            'films' => $films
         ]);
     }
+
+    
 }
