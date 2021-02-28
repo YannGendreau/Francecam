66b82f2ac035ca64ba926ec40cae326e6b952e3d
YannGendreau <yanngendreau@gmail.com>
2021-02-28 15:18:27 +0100

    NOUVELLE NAVBAR new layout et sauvegarde

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-// import './styles/liste.scss';
+// import './styles/menu.scss';
 // import './styles/film.scss';
 // import './styles/film2.scss';
 // import './styles/cartefilmfinal.scss';
