32aff84b111c445e88a76681fbad02d7988fb47e
Atome60 <q.hennion@gmail.com>
2021-03-30 11:03:05 +0200

    suite migration

--- migrations/Version05RefactoDBForFilm_camera.php
+++ migrations/Version05RefactoDBForFilm_camera.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class VersionRefactoDBForCamera extends AbstractMigration
+final class Version05RefactoDBForFilm_camera extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,19 +20,18 @@ final class VersionRefactoDBForCamera extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE IF NOT EXISTS `camera` (
-            `id` int NOT NULL AUTO_INCREMENT,
-            `marque_id` int DEFAULT NULL,
-            `modele_id` int DEFAULT NULL,
-            PRIMARY KEY (`id`),
-            KEY `IDX_3B1CEE054827B9B2` (`marque_id`),
-            KEY `IDX_3B1CEE05AC14B70A` (`modele_id`)
-          ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;');
+        $this->addSql('CREATE TABLE IF NOT EXISTS `film_camera` (
+            `film_id` int NOT NULL,
+            `camera_id` int NOT NULL,
+            PRIMARY KEY (`film_id`,`camera_id`),
+            KEY `IDX_50EED30F567F5183` (`film_id`),
+            KEY `IDX_50EED30FB47685CD` (`camera_id`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE camera');
+        $this->addSql('DROP TABLE film_camera');
     }
 }
