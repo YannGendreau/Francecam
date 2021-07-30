109547da8545892010a3631f4b38824c965a8b83
YannGendreau <yanngendreau@gmail.com>
2021-07-30 12:50:21 +0200

    HIDE ID ON ADMIN CRUD USER, CONDITION VERIFIED Chache l'id sur l'user admin crud, conditionne affichage de film avec isVerified

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -20,7 +20,7 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             TextField::new('name'),
             EmailField::new('email'),
             ArrayField::new('roles')
