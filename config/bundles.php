c6557b855f7dfe8f4361372f51c11f57b639166c
YannGendreau <yanngendreau@gmail.com>
2021-08-19 09:22:33 +0200

    INSTALL MERCURY FLASHY + UPDATE Met à jour et installe Flashy

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Captcha\Bundle\CaptchaBundle\CaptchaBundle::class => ['all' => true],
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
 ];
